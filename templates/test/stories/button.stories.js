6bf0866f158a03c8b3c4766b0b1d97589e4e80d9
Nando Vieira <me@fnando.com>
2019-02-13 23:25:35 -0800

    Update templates.

--- templates/test/stories/button.stories.js
+++ templates/test/stories/button.stories.js
@@ -2,24 +2,10 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-import PropTypes from "prop-types";
-
-class Button extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-    onClick: PropTypes.func
-  };
-
-  render() {
-    const { onClick, children } = this.props;
-    return (<button onClick={onClick}>{children}</button>);
-  }
-}
-
-storiesOf("Button", module)
+storiesOf("button", module)
   .add("with text", () => (
-    <Button onClick={action("clicked")}>Some button</Button>
+    <button onClick={action("clicked")}>Some button</button>
   ))
   .add("with some emoji", () => (
-    <Button onClick={action("clicked")}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+    <button onClick={action("clicked")}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></button>
   ));
